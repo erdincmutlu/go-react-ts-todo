00e3e25b594bbff5712f93b5af994e2b13fcedb4
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-01-11 13:16:05 +0000

    Implemented AddTodo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,24 @@ import useSWR from 'swr';
 import './App.css';
 import AddTodo from './components/AddTodo';
 
+export interface Todo {
+  id: number;
+  title: string;
+  body: string;
+  done: boolean;
+}
+
 export const ENDPOINT = 'http://localhost:4000'
 
 const fetcher = (url: string) =>
   fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
 
 function App() {
-  const {data, mutate} = useSWR('api/todos', fetcher)
-  return <MantineProvider>{JSON.stringify(data)}
+  const {data, mutate} = useSWR<Todo[]>('api/todos', fetcher)
+  return
+  <MantineProvider>{JSON.stringify(data)}
 
-  <AddTodo />
+    <AddTodo mutate={mutate} />
   </MantineProvider>;
 }
 
--- client/src/components/AddTodo.tsx
+++ client/src/components/AddTodo.tsx
@@ -1,8 +1,10 @@
 import {useState} from 'react';
 import {useForm} from '@mantine/form';
-import {Button, Modal, Group} from '@mantine/core';
+import {Button, Modal, Group, TextInput, Textarea} from '@mantine/core';
+import { ENDPOINT, Todo } from '../App';
+import { KeyedMutator } from 'swr';
 
-function AddTodo(){
+function AddTodo({mutate}: { mutate: KeyedMutator<Todo[]>}){
     const [open, setOpen] = useState(false)
 
     const form = useForm({
@@ -12,10 +14,41 @@ function AddTodo(){
         },
     });
 
+    async function createTodo(values: {title: string, body: string}) {
+      const updated = await fetch(`${ENDPOINT}/api/todos`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      }).then((r) => r.json());
+
+      mutate(updated)
+      form.reset();
+      setOpen(false);
+    }
+
     return ( 
       <>
         <Modal opened={open} onClose={() => setOpen(false)} title="Create Todo">
-          text
+          <form onSubmit={form.onSubmit(createTodo)}>
+            <TextInput
+              required
+              mb={12}
+              label="Todo"
+              placeholder="What do you want to do?"
+              {...form.getInputProps("title")}
+            />
+            <Textarea
+              required
+              mb={12}
+              label="Body"
+              placeholder="Tell me more"
+              {...form.getInputProps("body")}
+            />
+
+            <Button type="submit">Create Todo</Button>
+          </form>
         </Modal>
         <Group position="center">
             <Button fullWidth mb={12} onClick={() => setOpen(true)}>
